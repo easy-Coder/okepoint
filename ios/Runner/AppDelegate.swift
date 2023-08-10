51ccc53bc9c32971010a4a573cb7df7ff2770765
Paul Edeme'kong <68385854+edemekong@users.noreply.github.com>
2023-08-10 11:45:31 +0100

    remove unused plugin

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,6 @@ import UIKit
 import Flutter
 import GoogleMaps
 import Firebase
-import flutter_background_service_ios
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -12,8 +11,6 @@ import flutter_background_service_ios
   ) -> Bool {
     GMSServices.provideAPIKey("YOUR-API-KEY")
     FirebaseApp.configure()
-    SwiftFlutterBackgroundServicePlugin.taskIdentifier = "dev.dashlingo.okepoint-dev"
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
